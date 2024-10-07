055be5f2a12ea74c638c893d16ad7f417ac343c2
JoshuaXvmx <jdmgetrouw@gmail.com>
2024-10-07 04:33:33 +0200

    revamp

---
+++ Assets/scripts/Switch.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    public GameObject Collectable1;
+    public GameObject Collectable2;
+    public GameObject Daytime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+// if both collectables are taken then switch scene to day time
+    }
+}
